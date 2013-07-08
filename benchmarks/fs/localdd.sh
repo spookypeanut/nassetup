a0d81956d3865405a5b5795002997e40ac3b3db4
spookypeanut <github@spookypeanut.co.uk>
2013-07-08 23:14:30 +0100

    Put the results in a nicer place

--- benchmarks/fs/localdd.sh
+++ benchmarks/fs/localdd.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
+DATETIME=$(date +%s)
+RESULTDIR=results/$DATETIME
+mkdir -p $RESULTDIR
+
 for i in $(cat types.txt); do
     echo Doing $i
-    LOG=$i.timings.txt
-    rm $LOG
+    LOG=$RESULTDIR/$i.timings.txt
     echo dd output >> $LOG
     OUTFILE=/mnt/$i/output.img
     rm $OUTFILE
