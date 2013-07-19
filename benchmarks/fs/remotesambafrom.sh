2b24e6a94211c0a73d0eca985b1e835a9a71aeb3
spookypeanut <github@spookypeanut.co.uk>
2013-07-19 23:49:37 +0100

    MORE TESTS!

---
+++ benchmarks/fs/remotesambafrom.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DATETIME=$(date +%s)
+RESULTDIR=results/remotesambafrom/$DATETIME
+mkdir -p $RESULTDIR
+
+for i in $(cat types.txt); do
+    echo Doing $i
+    rm /tmp/largefilm.m4v
+    LOG=$RESULTDIR/$i.txt
+    SRC=/mnt/spookybackup/$i/largefilm.m4v
+    DEST=/tmp/
+    (time rsync -vv $SRC $DEST) &>> $LOG
+    sync
+    echo "Sleeping for 1 minute"
+    sleep 1m
+done
