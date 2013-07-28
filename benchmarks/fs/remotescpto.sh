ad47c867e115e087326c3e410ef0d566add1c83e
spookypeanut <github@spookypeanut.co.uk>
2013-07-28 23:44:58 +0100

    Cleanup of scp to output

--- benchmarks/fs/remotescpto.sh
+++ benchmarks/fs/remotescpto.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 DATETIME=$(date +%s)
-RESULTDIR=results/remotescp/$DATETIME
+RESULTDIR=results/remotescpto/$DATETIME
 FILE=tiny.mp3
 mkdir -p $RESULTDIR
 
 for i in $(cat types.txt); do
     echo Doing $i
-    LOG=$RESULTDIR/$i.scp.txt
+    LOG=$RESULTDIR/$i.txt
     SRC=../../data/$FILE
     DEST=spookybackup:/mnt/$i/
     (time scp $SRC $DEST) &>> $LOG
