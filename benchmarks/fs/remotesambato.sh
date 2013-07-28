1f5f8987f02133cc2f5eb8db7957fa0369238319
spookypeanut <github@spookypeanut.co.uk>
2013-07-28 23:51:47 +0100

    Cleanup of samba to output

--- benchmarks/fs/remotesambato.sh
+++ benchmarks/fs/remotesambato.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 DATETIME=$(date +%s)
-RESULTDIR=results/remotesamba/$DATETIME
+RESULTDIR=results/remotesambato/$DATETIME
 mkdir -p $RESULTDIR
+FILE=tiny.mp3
 
 for i in $(cat types.txt); do
     echo Doing $i
-    LOG=$RESULTDIR/$i.samba.txt
-    SRC=../../data/largefilm.m4v
+    LOG=$RESULTDIR/$i.txt
+    SRC=../../data/$FILE
     DEST=/mnt/spookybackup/$i/
     (time rsync -vv $SRC $DEST) &>> $LOG
     sync
